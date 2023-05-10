f23ea25a22acf21e66ac022b3586ac522e7c0e0c
Icecubesaad <saad.qad418@gmail.com>
2023-05-11 02:30:04 +0500

    implementing liking system

--- client/BlogApp/src/components/ShowBlogs.jsx
+++ client/BlogApp/src/components/ShowBlogs.jsx
@@ -6,8 +6,7 @@ import SidebarBlogs from "./SideComponent/SidebarBlogs";
 import Header from "./Header";
 const ShowBlogs = () => {
   const [checked, setchecked] = useState(false);
-  
-  
+
   const [ShowBlogs, setShowBlogs] = useState("");
   const [changelike, setchangelike] = useState(true);
   const [likes, setlikes] = useState();
@@ -16,6 +15,7 @@ const ShowBlogs = () => {
     fetchBlog();
   }, [id]);
   const fetchBlog = async () => {
+    
     const data = await fetch(`/api/blogs/filter/${id}`, {
       method: "GET",
     });
@@ -26,6 +26,7 @@ const ShowBlogs = () => {
   const UpdateLike = async () => {
     setchangelike(false);
     setlikes((e) => e + 1);
+    updateUser(changelike);
     const data = await fetch(`/api/blogs/updates/${id}`, {
       method: "POST",
       headers: {
@@ -36,8 +37,10 @@ const ShowBlogs = () => {
       }),
     });
   };
+  
   const Dislike = async () => {
-    setchangelike(true);
+    setchangelike(true)
+    updateUserDisLike()
     setlikes((e) => e - 1);
     const data = await fetch(`/api/blogs/updates/${id}`, {
       method: "POST",
@@ -49,6 +52,30 @@ const ShowBlogs = () => {
       }),
     });
   };
+  const updateUser = async()=>{
+    const User_side = await fetch(`/api/auth/likes/${id}`,{
+      method:"POST",
+      headers:{
+        "jwt_token" : localStorage.getItem("key"),
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        option:true
+      })
+    })
+}
+const updateUserDisLike = async()=>{
+  const User_side = await fetch(`/api/auth/likes/${id}`,{
+    method:"POST",
+    headers:{
+      "jwt_token" : localStorage.getItem("key"),
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify({
+      option:false
+    })
+  })
+}
   return (
     <>
       <Header />
@@ -70,7 +97,7 @@ const ShowBlogs = () => {
               width: "80%",
               marginTop: "20px",
               flexDirection: "row",
-              justifyContent:"space-between"
+              justifyContent: "space-between",
             }}
           >
             <div
@@ -86,14 +113,19 @@ const ShowBlogs = () => {
             <div
               style={{
                 display: "flex",
-                marginLeft:"70px",
-                alignItems:"center"
+                marginLeft: "70px",
+                alignItems: "center",
               }}
             >
               {likes}
               <div>
-                { changelike ? <button style={{background:"transparent",border:"none"}} onClick={UpdateLike}>
-                        <input type="checkbox" id="checkbox" />
+                {changelike ? (
+                  <button
+                  style={{backgroundColor:"transparent",border:"transparent"}}
+                    
+                    onClick={UpdateLike}
+                  >
+                    <input type="checkbox" id="checkbox" />
                         <label for="checkbox">
                           <svg
                             id="heart-svg"
@@ -269,185 +301,190 @@ const ShowBlogs = () => {
                             </g>
                           </svg>
                         </label>
-                      </button> : <button style={{background:"transparent",border:"none"}} onClick={Dislike}>
-                        <input type="checkbox" id="checkbox" />
-                        <label for="checkbox">
-                          <svg
-                            id="heart-svg"
-                            viewBox="467 392 58 57"
-                            xmlns="http://www.w3.org/2000/svg"
+                  </button>
+                ) : (
+                  <button onClick={Dislike} style={{backgroundColor:"transparent",border:"transparent", marginLeft:"15px"}}><div id="main-content">
+                    <div>
+                      <input type="checkbox" id="checkbox1" />
+                      <label for="checkbox1">
+                        <svg
+                          id="heart-svg"
+                          viewBox="467 392 58 57"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g
+                            id="Group"
+                            fill="red"
+                            fill-rule="evenodd"
+                            transform="translate(467 392)"
                           >
+                            <path
+                              d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z"
+                              id="heart"
+                              fill="red"
+                            />
+                            <circle
+                              id="main-circ"
+                              fill="#E2264D"
+                              opacity="0"
+                              cx="29.5"
+                              cy="29.5"
+                              r="1.5"
+                            />
+
                             <g
-                              id="Group"
-                              fill="none"
-                              fill-rule="evenodd"
-                              transform="translate(467 392)"
+                              id="grp7"
+                              opacity="0"
+                              transform="translate(7 6)"
                             >
-                              <path
-                                d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z"
-                                id="heart"
-                                fill="#AAB8C2"
+                              <circle
+                                id="oval1"
+                                fill="#9CD8C3"
+                                cx="2"
+                                cy="6"
+                                r="2"
                               />
                               <circle
-                                id="main-circ"
-                                fill="#E2264D"
-                                opacity="0"
-                                cx="29.5"
-                                cy="29.5"
-                                r="1.5"
+                                id="oval2"
+                                fill="#8CE8C3"
+                                cx="5"
+                                cy="2"
+                                r="2"
                               />
-
-                              <g
-                                id="grp7"
-                                opacity="0"
-                                transform="translate(7 6)"
-                              >
-                                <circle
-                                  id="oval1"
-                                  fill="#9CD8C3"
-                                  cx="2"
-                                  cy="6"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval2"
-                                  fill="#8CE8C3"
-                                  cx="5"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp6"
-                                opacity="0"
-                                transform="translate(0 28)"
-                              >
-                                <circle
-                                  id="oval1"
-                                  fill="#CC8EF5"
-                                  cx="2"
-                                  cy="7"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval2"
-                                  fill="#91D2FA"
-                                  cx="3"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp3"
-                                opacity="0"
-                                transform="translate(52 28)"
-                              >
-                                <circle
-                                  id="oval2"
-                                  fill="#9CD8C3"
-                                  cx="2"
-                                  cy="7"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval1"
-                                  fill="#8CE8C3"
-                                  cx="4"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp2"
-                                opacity="0"
-                                transform="translate(44 6)"
-                              >
-                                <circle
-                                  id="oval2"
-                                  fill="#CC8EF5"
-                                  cx="5"
-                                  cy="6"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval1"
-                                  fill="#CC8EF5"
-                                  cx="2"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp5"
-                                opacity="0"
-                                transform="translate(14 50)"
-                              >
-                                <circle
-                                  id="oval1"
-                                  fill="#91D2FA"
-                                  cx="6"
-                                  cy="5"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval2"
-                                  fill="#91D2FA"
-                                  cx="2"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp4"
-                                opacity="0"
-                                transform="translate(35 50)"
-                              >
-                                <circle
-                                  id="oval1"
-                                  fill="#F48EA7"
-                                  cx="6"
-                                  cy="5"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval2"
-                                  fill="#F48EA7"
-                                  cx="2"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
-
-                              <g
-                                id="grp1"
-                                opacity="0"
-                                transform="translate(24)"
-                              >
-                                <circle
-                                  id="oval1"
-                                  fill="#9FC7FA"
-                                  cx="2.5"
-                                  cy="3"
-                                  r="2"
-                                />
-                                <circle
-                                  id="oval2"
-                                  fill="#9FC7FA"
-                                  cx="7.5"
-                                  cy="2"
-                                  r="2"
-                                />
-                              </g>
                             </g>
-                          </svg>
-                        </label>
-                      </button>}
-                      </div>
+
+                            <g
+                              id="grp6"
+                              opacity="0"
+                              transform="translate(0 28)"
+                            >
+                              <circle
+                                id="oval1"
+                                fill="#CC8EF5"
+                                cx="2"
+                                cy="7"
+                                r="2"
+                              />
+                              <circle
+                                id="oval2"
+                                fill="#91D2FA"
+                                cx="3"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+
+                            <g
+                              id="grp3"
+                              opacity="0"
+                              transform="translate(52 28)"
+                            >
+                              <circle
+                                id="oval2"
+                                fill="#9CD8C3"
+                                cx="2"
+                                cy="7"
+                                r="2"
+                              />
+                              <circle
+                                id="oval1"
+                                fill="#8CE8C3"
+                                cx="4"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+
+                            <g
+                              id="grp2"
+                              opacity="0"
+                              transform="translate(44 6)"
+                            >
+                              <circle
+                                id="oval2"
+                                fill="#CC8EF5"
+                                cx="5"
+                                cy="6"
+                                r="2"
+                              />
+                              <circle
+                                id="oval1"
+                                fill="#CC8EF5"
+                                cx="2"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+
+                            <g
+                              id="grp5"
+                              opacity="0"
+                              transform="translate(14 50)"
+                            >
+                              <circle
+                                id="oval1"
+                                fill="#91D2FA"
+                                cx="6"
+                                cy="5"
+                                r="2"
+                              />
+                              <circle
+                                id="oval2"
+                                fill="#91D2FA"
+                                cx="2"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+
+                            <g
+                              id="grp4"
+                              opacity="0"
+                              transform="translate(35 50)"
+                            >
+                              <circle
+                                id="oval1"
+                                fill="#F48EA7"
+                                cx="6"
+                                cy="5"
+                                r="2"
+                              />
+                              <circle
+                                id="oval2"
+                                fill="#F48EA7"
+                                cx="2"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+
+                            <g
+                              id="grp1"
+                              opacity="0"
+                              transform="translate(24)"
+                            >
+                              <circle
+                                id="oval1"
+                                fill="#9FC7FA"
+                                cx="2.5"
+                                cy="3"
+                                r="2"
+                              />
+                              <circle
+                                id="oval2"
+                                fill="#9FC7FA"
+                                cx="7.5"
+                                cy="2"
+                                r="2"
+                              />
+                            </g>
+                          </g>
+                        </svg>
+                      </label>
+                    </div>
+                  </div></button>
+                )}
+              </div>
             </div>
           </div>
           <div
--- server/Router/Auth.js
+++ server/Router/Auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const UserModel = require("../Model/Auth");
 const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const jwt_secret = "ICECUBE";
-const middleware = require("../Middleware/Middleware")
+const middleware = require("../Middleware/Middleware");
 router.post(
   "/Signup",
   [
@@ -35,8 +35,7 @@ router.post(
         return res.status(405).json({ emailErrors });
       } else if (passwordErrors.length > 0) {
         return res.status(401).json({ passwordErrors });
-      }
-       else if (nameErrors.length > 0) {
+      } else if (nameErrors.length > 0) {
         return res.status(403).json({ nameErrors });
       }
     }
@@ -49,47 +48,92 @@ router.post(
         if (User_replica) {
           res.send({ error: "Username is already taken" });
         } else {
-            const salt = await bcrypt.genSalt(10);
-            const parsedPass = await bcrypt.hash(password,salt)
+          const salt = await bcrypt.genSalt(10);
+          const parsedPass = await bcrypt.hash(password, salt);
           const Saving = await UserModel.create({
             Email: email,
             Password: parsedPass,
             UserName: name,
           });
-          res.send(Saving)
+          res.send(Saving);
         }
       }
     } catch (error) {
-      res.send({error});
+      res.send({ error });
     }
-
   }
 );
-router.post("/Signin",async(req,res)=>{
-    const {email,password} = req.body;
-    const User_auth = await UserModel.findOne({Email:email})
-    if(!User_auth){
-        res.status(402).json({error:"Invald credetials"})
+router.post("/Signin", async (req, res) => {
+  const { email, password } = req.body;
+  const User_auth = await UserModel.findOne({ Email: email });
+  if (!User_auth) {
+    res.status(402).json({ error: "Invald credetials" });
+  } else {
+    const valid = await bcrypt.compare(password, User_auth.Password);
+    if (valid) {
+      const User = {
+        User_Id: {
+          ID: User_auth.id,
+        },
+      };
+      const Token = await jwt.sign(User, jwt_secret);
+      res.json(Token);
+    } else {
+      res.send({ error: "invalid credidential" });
     }
-    else{
-        const valid = await bcrypt.compare(password,User_auth.Password)
-        if(valid){
-            const User={
-                User_Id:{
-                    ID:User_auth.id
-                }
+  }
+});
+router.get("/Get", middleware, async (req, res) => {
+  const User_id = req.user.ID;
+  const User_details = await UserModel.findById(User_id);
+  res.send(User_details);
+});
+router.post("/likes/:id", middleware, async (req, res) => {
+  const user_ID = req.user.ID;
+  const Blog_id = req.params.id;
+  const { option } = req.body;
+  console.log(
+    "options : ",
+    option,
+    " Blog ID : ",
+    Blog_id,
+    " User Id : ",
+    user_ID
+  );
+  const userData = await UserModel.findOne({ _id: user_ID });
+  if (!userData) {
+    return res.status(404).send("User not found");
+  } else {
+    if (option) {
+      if (userData.Liked.includes(Blog_id)) {
+        return res.status(400).json({ message: "id already exist " });
+      }
+      else{
+        try {
+              const updating_Likes = await UserModel.findOneAndUpdate(
+                { _id: user_ID },
+                { $push: { Liked: Blog_id } },
+                { new: true }
+              );
+            } catch (error) {
+              res.send.json(error);
+              console.log(error);
             }
-            const Token = await jwt.sign(User,jwt_secret)
-            res.json(Token)
-        }
-        else{
-            res.send({error:"invalid credidential"})
-        }
+      }
+      }
+      if (!option) {
+          try {
+            const updating_Likes = await UserModel.findOneAndUpdate(
+              { _id: user_ID },
+              { $pull: { Liked: Blog_id } },
+              { new: true }
+            );
+          } catch (error) {
+            res.send.json(error);
+            console.log(error);
+          }
     }
-})
-router.get("/Get",middleware,async(req,res)=>{
-    const User_id = req.user.ID;
-    const User_details = await UserModel.findById(User_id)
-    res.send(User_details)
-})
+  }
+});
+
 module.exports = router;
