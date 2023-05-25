8c5ac0513c01578311f747cb5f4f2ec4842c1f4b
Icecubesaad <saad.qad418@gmail.com>
2023-05-25 22:48:27 +0500

    .server

--- server/App.js
+++ server/App.js
@@ -1,20 +1,25 @@
-const express = require("express")
-const app = express()
-const database = require("./DB/Database")
-const router = express.Router()
-database()
+const express = require("express");
+const app = express();
+const database = require("./DB/Database");
+const router = express.Router();
+const path = require("path");
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ limit: '500mb', extended: true, parameterLimit: 100000 }));
-app.use(express.json({ limit: '500mb' }));
+database();
 
-const port = 3000
-router.get("/",(req,res)=>{
-    res.send("Hello niggas")
-})
-app.use("/api/blogs",require("./Router/Blogs"))
-app.use("/api/auth",require("./Router/Auth"))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ limit: "500mb", extended: true, parameterLimit: 100000 }));
+app.use(express.json({ limit: "500mb" }));
 
-app.listen(port,()=>{
-    console.log(`server running at ${port}`)
-})
+const port = process.env.PORT || 3000;
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use("/", router);
+app.use("/api/blogs", require("./Router/Blogs"));
+app.use("/api/auth", require("./Router/Auth"));
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
