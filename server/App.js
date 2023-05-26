a8ea38c73251621c37481de73d4e37e722302241
Icecubesaad <saad.qad418@gmail.com>
2023-05-26 23:53:14 +0500

    testing2

--- server/App.js
+++ server/App.js
@@ -1,24 +1,7 @@
-const express = require("express");
-const app = express();
-const database = require("./DB/Database");
-const router = express.Router();
-const path = require("path");
+const express = require("express")
+const app = require("./server");
 
-database();
+module.exports = (req, res) => {
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ limit: "500mb", extended: true, parameterLimit: 100000 }));
-app.use(express.json({ limit: "500mb" }));
-
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-app.use("/api/blogs", require("./Router/Blogs"));
-app.use("/api/auth", require("./Router/Auth"));
-
-app.listen(port, () => {
-  console.log(`Server running at ${port}`);
-});
-module.exports = app;
+  app(req, res);
+};
