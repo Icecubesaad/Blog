8b59f8a070780449a295d07d269a7f1d48a1f805
Icecubesaad <saad.qad418@gmail.com>
2023-09-30 06:10:27 -0700

    sever optimized

--- server/App.js
+++ server/App.js
@@ -1,7 +1,38 @@
-const express = require("express")
-const app = require("./server");
+const express = require("express");
+const app = express();
+const database = require("./DB/Database");
+const path = require("path");
+const cors= require('cors')
+// Initialize database connection
+database();
 
-module.exports = (req, res) => {
+// Use express.json() directly
+app.use(express.json({ limit: "500mb" }));
+app.use(cors(
+  {
+      origin: ['https://blog-app-omega-olive.vercel.app/'],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
+const port = process.env.PORT || 3000;
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err); // Log the error for debugging purposes
+  
+    // Set the response status to 500
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
+  
+  // Rest of your code...
+  
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+app.use("/api/blogs", require("./Router/Blogs"));
+app.use("/api/auth", require("./Router/Auth"));
 
-  app(req, res);
-};
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
+module.exports = app;
